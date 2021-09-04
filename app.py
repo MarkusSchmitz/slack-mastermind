49de63a7edf0be95d44bcead471f071d25da79ab
Markus Schmitz <markus.schmitz@fau.de>
2021-09-04 13:35:31 +0200

    added endpint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 
@@ -7,5 +8,20 @@ def hello():
     return "Hello, Sebastian!"
 
 
+@app.route('/slack-endpoint', methods=['POST', "GET"])
+def endpoint():
+    
+    with open("log.txt", "wb") as f:
+        f.write(request.data)
+
+    return str(request.data)
+        
+    #else:
+        # You probably don't have args at this route with GET
+        # method, but if you do, you can access them like so:
+    #    yourarg = flask.request.args.get('argname')
+    #    your_register_template_rendering(yourarg)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
