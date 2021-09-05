4cf150f9052aa1f9539d569393eff87feebb556a
Markus Schmitz <markus.ms.schmitz@bmwgroup.com>
2021-09-05 08:19:20 +0200

    updated repsone

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
-from flask import request
+from flask import Flask, jsonify, request
+from transformers import pipeline
+import json
+
+
 app = Flask(__name__)
 
+pipe = pipeline('text-generation', model="dbmdz/german-gpt2",
+                 tokenizer="dbmdz/german-gpt2")
 
 @app.route("/")
 def hello():
@@ -14,7 +19,11 @@ def endpoint():
     with open("log.txt", "ab") as f:
         f.write(request.data)
 
-    return str(request.data)
+    answer = {
+        "text" : "Random Text"
+    }
+
+    return jsonify(answer)
         
     #else:
         # You probably don't have args at this route with GET
@@ -22,6 +31,13 @@ def endpoint():
     #    yourarg = flask.request.args.get('argname')
     #    your_register_template_rendering(yourarg)
 
+def predict(text):
+    
+
+    text = pipe("Der Sinn des Lebens ist es", max_length=100)[0]["generated_text"]
+    return text
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+    
