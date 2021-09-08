ed54a673c9f865e12a7f39d3cc163eec5bb2d1da
Markus Schmitz <markus.schmitz@fau.de>
2021-09-08 20:51:28 +0200

    changed regular

--- app.py
+++ app.py
@@ -14,8 +14,19 @@ model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large")
 
 @app.route("/")
 def hello():
-    logger.info("Returned static")
-    return "Hello, Sebastian!"
+    new_user_input_ids = tokenizer.encode("How are you doing?" + tokenizer.eos_token, return_tensors='pt')
+
+    # append the new user input tokens to the chat history
+    bot_input_ids = new_user_input_ids
+
+    # generated a response while limiting the total chat history to 1000 tokens, 
+    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+    # pretty print last ouput tokens from bot
+    answer = ("{}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
+
+    logger.info("responding with {answer}")
+    return jsonify(answer)
 
 
 
